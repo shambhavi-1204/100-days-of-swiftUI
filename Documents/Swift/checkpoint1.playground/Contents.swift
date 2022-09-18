a7426f76c7372c9cb09b9560bc34659aa8072a8a
Shambhavi Jha <sham.120401@gmail.com>
2022-09-18 19:13:48 +0530

    adding relevant files

---
+++ Documents/Swift/checkpoint1.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Cocoa
+
+// MARK: - CHECKPOINT 1 - SIMPLE DATA TYPES
+// Making a simple temperature converter from celsius to farhenheit
+
+let celsius: Double = 32.0
+let fahrenheit: Double
+
+fahrenheit = 9/5 * celsius + 32
+print("Temperature in celsius: \(celsius)C, Temperature in fahrenheit: \(fahrenheit)F.")
