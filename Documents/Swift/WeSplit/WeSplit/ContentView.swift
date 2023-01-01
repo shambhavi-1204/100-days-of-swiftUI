469de3303457d143f31749f63b43bbea4897a46b
Shambhavi Jha <sham.120401@gmail.com>
2023-01-01 21:38:44 +0530

    Conditional modifier added

--- Documents/Swift/WeSplit/WeSplit/ContentView.swift
+++ Documents/Swift/WeSplit/WeSplit/ContentView.swift
@@ -56,6 +56,7 @@ struct ContentView: View {
                 
                 Section {
                     Text(totalAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .foregroundColor(tipPercentage==0 ? .red : .black)
                 } header: {
                     Text("Grand Total")
                 }
